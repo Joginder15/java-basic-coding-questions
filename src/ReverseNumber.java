bab091fe47715f2da84be4fb5a2fc56a6e999f26
joginder <joginder.kumar@cogneesol.net>
2025-12-07 11:50:34 +0530

    ReverseNumber program done

---
+++ src/ReverseNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input a number: ");
+        int number = scanner.nextInt();
+
+        int reverse = 0;
+        while(number > 0){
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+
+        System.out.println("Reverse of the given number is: " + reverse);
+    }
+}
