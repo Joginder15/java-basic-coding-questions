4128c5181d45dfb44c9d1711f5ffde7c99eaf15e
joginder <joginder.kumar@cogneesol.net>
2025-12-08 15:54:30 +0530

    DecimalToBinary program done

---
+++ src/DecimalToBinary.java
@@ -0,0 +1,8 @@
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        int n = 10;
+
+        String binaryString = Integer.toBinaryString(n);
+        System.out.println("Binary string is " + binaryString);
+    }
+}
