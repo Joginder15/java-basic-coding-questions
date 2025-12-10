ebc6d8fc96a3291bf691303abb27aa487f755854
joginder <joginder.kumar@cogneesol.net>
2025-12-10 13:45:28 +0530

    SumOfDigits program done

---
+++ src/SumOfDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int input = scanner.nextInt();
+
+        int sum = 0;
+        while(input > 0){
+            int remainder = input % 10;
+            sum += remainder;
+            input = input /10;
+        }
+
+        System.out.println(sum);
+    }
+}
